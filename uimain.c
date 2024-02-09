c28e21da4dd09beeefa4ae2c11c718b477df8274
Naramirez38 <naramirez38@miners.utep.edu>
2024-02-08 17:48:45 -0700

    Almost complete just needs right input names

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
   //hello
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a, Chars = c, 5x7 Chars = n) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -28,6 +28,11 @@ int main()
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
+      break;
+    case 'n':
+      puts("You chose 5x7:");
+      for(char c = 'A'; c < 'D'; c++)
+	print_char_5x7(c);
       break;
     case 'a':
       puts("You selected arrow:");
